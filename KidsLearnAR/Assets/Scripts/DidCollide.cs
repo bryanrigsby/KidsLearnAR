a63293e7ed62cc6c5d79b8df98d3d45b4c851e3b
Bryan Rigsby <bryanrigsby@att.net>
2021-06-05 19:01:17 -0500

    initial commit

---
+++ KidsLearnAR/Assets/Scripts/DidCollide.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DidCollide : MonoBehaviour
+{
+
+    private string[] colors = { "Red Cube", "Blue Cube", "White Cube" };
+    private int randNum;
+    public Text text;
+    private string goalCube;
+    public int score = 0;
+    public Text scoreText;
+
+    private void Start()
+    {
+        randNum = Random.Range(0, colors.Length);
+        goalCube = colors[randNum];
+        Debug.Log(goalCube);
+        text.text = "Run to the " + goalCube;
+        scoreText.text = score.ToString();
+    }
+
+    private void Update()
+    {
+        //testing
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            score += 10;
+            scoreText.text = score.ToString();
+        }   
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == goalCube)
+        {
+            text.text = "Correct! That is the " + goalCube + "!";
+            Debug.Log(goalCube);
+
+            score += 10;
+            scoreText.text = score.ToString();
+        }
+        else
+        {
+            text.text = "Nope! That's the " + collision.tag + ".  Try again!";
+            score -= 1;
+            scoreText.text = score.ToString();
+        }
+
+        if(collision.tag == "Red Cube")
+        {
+            Debug.Log("Red Cube");
+        }
+        else if(collision.tag == "Blue Cube")
+        {
+            Debug.Log("Blue Cube");
+        }
+        else if(collision.tag == "White Cube")
+        {
+            Debug.Log("White Cube");
+        }
+    }
+
+    
+}
---
+++ KidsLearnAR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int score = 0;
+
+    public Text scoreText;
+
+    public static PlayerController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
